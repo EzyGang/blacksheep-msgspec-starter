225cdd0348a4cccd28738764d6ec1409afa6b72b
Galtozzy <galtozzy@Galtozzys-MacBook-Air.local>
2024-09-18 13:14:27 +0200

    using msgspec json decoder

--- app/lib/binders.py
+++ app/lib/binders.py
@@ -3,6 +3,7 @@ from typing import Any, TypeVar
 import msgspec.msgpack
 from blacksheep import Request
 from blacksheep.server.bindings import Binder, BoundValue
+from blacksheep.settings.json import json_settings
 
 
 T = TypeVar('T')
@@ -22,3 +23,11 @@ class MsgPackBinder(Binder):
         if not (content := await request.read()):
             return None
         return msgspec.msgpack.decode(content, type=self.expected_type)
+
+
+json_decoder = msgspec.json.Decoder()
+
+
+json_settings.use(  # type: ignore[no-untyped-call]
+    loads=json_decoder.decode,
+)
